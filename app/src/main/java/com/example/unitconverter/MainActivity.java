daab7f12ac14d83137552231612c64e16594c73c
Mrugesh <mrugeshrlimbachiya@gmail.com>
2024-10-15 15:34:45 +0530

    feat: validations and error handling added

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.unitconverter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+    }
+    public void calculate(View view) {
+        String s = editText.getText().toString().trim();
+        if (s.isEmpty()) {
+            Toast.makeText(this, "Please enter a value", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        try {
+            double kg = Double.parseDouble(s);
+            double pound = 2.20462 * kg;
+            textView.setText("The corresponding value in Pounds is " + String.format("%.2f", pound));
+            Toast.makeText(this, "Thanks for using this app", Toast.LENGTH_SHORT).show();
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_SHORT).show();
+        }
     }
 }
